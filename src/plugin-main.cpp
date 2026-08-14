41a320ed149d32fc9e27eed0307361cd0bb97c85
José Almeida <jose.afga@gmail.com>
2026-08-14 19:02:45 -0300

    uniform dock id

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -30,7 +30,7 @@ bool obs_module_load(void)
 	ix::initNetSystem();
 
 	CounterDock *streamiau_counter_dock = new CounterDock();
-	bool success = obs_frontend_add_dock_by_id("streamiau-counter-dock_id",
+	bool success = obs_frontend_add_dock_by_id("streamiau_counter_dock_id",
 						   obs_module_text("StreamiauCounter.Title"), streamiau_counter_dock);
 
 	if (success) {
